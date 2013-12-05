5fe141677ae71b52e999f7d952d8a3b4bc707988
John Mitsch <johnpmitsch@gmail.com>
2013-12-05 05:18:12 -0500

    wrote some of the code for the guessing javascript

---
+++ app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    //$('img').fadeIn(3000);
+	//$('#statement1').delay(2000).fadeIn(1000);
+	//$('#statement2').delay(4000).fadeIn(1000);
+	//$('#guess').delay(6000).fadeIn(1000);
+	//$('.button').delay(6000).fadeIn(1000);
+	//$('.buttonTwo').delay(6000).fadeIn(1000);
+		
+	var randomNumber = Math.floor(Math.random() * 101);
+	var guessResult = 0;
+	var highlow=0;
+
+	$('#guessbutton').click(function() {
+		var userGuess = document.getElementById("guess").value;
+		var abs=Math.abs(userGuess-randomNumber);
+		if((userGuess == '') || (isNaN(userGuess)) || (userGuess < 1) || (userGuess > 100)) {	
+			guessResult = 'invalid';	
+			};
+		if(abs>15) {
+			guessResult="cold";
+		} else if ((abs>0)&&(abs<15)) {
+			guessResult="warm";
+			};
+		if(abs == 0) {
+			guessResult="correct";
+			};
+		if(userGuess<randomNumber) {
+			highlow=1;
+		} else if (userGuess>randomNumber) {
+			highlow=2;
+		}
+		
+			
+		alert(abs);
+		alert(highlow);
+	});
+	
+		
+});
